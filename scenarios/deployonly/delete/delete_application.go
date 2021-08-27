b07d1280bd755cbc801af6ce66a3a97a51315d5a
Nils Gustav Stråbø <nst@equinor.com>
2021-08-27 09:11:30 +0200

    add comment

--- scenarios/deployonly/delete/delete_application.go
+++ scenarios/deployonly/delete/delete_application.go
@@ -14,7 +14,10 @@ var logger *log.Entry
 // Applications Tests that we are able to delete applications
 func Applications(env env.Env, suiteName string) (bool, error) {
 	logger = log.WithFields(log.Fields{"Suite": suiteName})
+
+	// HACK Try sleeping for 60 seconds to hopefully preventing kubed from crashing
 	logger.Info("Sleeping for 60 seconds before deleteing application to prevent kubed from crashing")
 	time.Sleep(60 * time.Second)
+
 	return application.Delete(env, config.App3Name)
 }
