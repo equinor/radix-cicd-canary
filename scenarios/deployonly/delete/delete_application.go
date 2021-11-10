2d462cb060b8c3af16e9f7a11b3919ca1acd0046
Nils Gustav Stråbø <nst@equinor.com>
2021-11-10 09:58:54 +0100

    remove 60 sec sleep

--- scenarios/deployonly/delete/delete_application.go
+++ scenarios/deployonly/delete/delete_application.go
@@ -1,8 +1,6 @@
 package delete
 
 import (
-	"time"
-
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/application"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/config"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/env"
@@ -14,10 +12,5 @@ var logger *log.Entry
 // Applications Tests that we are able to delete applications
 func Applications(env env.Env, suiteName string) (bool, error) {
 	logger = log.WithFields(log.Fields{"Suite": suiteName})
-
-	// HACK Try sleeping for 60 seconds to hopefully prevent kubed from crashing
-	logger.Info("Sleeping for 60 seconds before deleteing application to prevent kubed from crashing")
-	time.Sleep(60 * time.Second)
-
 	return application.Delete(env, config.App3Name)
 }
