480738ab50ee15cbeabe744354ccc132dea56c85
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2021-11-10 10:09:24 +0100

    Revert sleep (#82)
    * remove 60 sec sleep
    
    * remove unused logging
    
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

--- scenarios/deployonly/delete/delete_application.go
+++ scenarios/deployonly/delete/delete_application.go
@@ -1,23 +1,12 @@
 package delete
 
 import (
-	"time"
-
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/application"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/config"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/env"
-	log "github.com/sirupsen/logrus"
 )
-
-var logger *log.Entry
 
 // Applications Tests that we are able to delete applications
 func Applications(env env.Env, suiteName string) (bool, error) {
-	logger = log.WithFields(log.Fields{"Suite": suiteName})
-
-	// HACK Try sleeping for 60 seconds to hopefully prevent kubed from crashing
-	logger.Info("Sleeping for 60 seconds before deleteing application to prevent kubed from crashing")
-	time.Sleep(60 * time.Second)
-
 	return application.Delete(env, config.App3Name)
 }
