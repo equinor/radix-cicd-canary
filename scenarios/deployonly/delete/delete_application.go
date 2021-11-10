74da6a59216554327dab95979112209a2cf45c9b
Nils Gustav Stråbø <nst@equinor.com>
2021-11-10 09:59:46 +0100

    remove unused logging

--- scenarios/deployonly/delete/delete_application.go
+++ scenarios/deployonly/delete/delete_application.go
@@ -4,13 +4,9 @@ import (
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/application"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/config"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/env"
-	log "github.com/sirupsen/logrus"
 )
-
-var logger *log.Entry
 
 // Applications Tests that we are able to delete applications
 func Applications(env env.Env, suiteName string) (bool, error) {
-	logger = log.WithFields(log.Fields{"Suite": suiteName})
 	return application.Delete(env, config.App3Name)
 }
