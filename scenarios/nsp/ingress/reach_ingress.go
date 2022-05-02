137765b36c222c083a3378cc9a9d1dee56367868
Magnus Longva <magnus@longva.email>
2022-05-02 16:41:19 +0200

    moving NSP success/error to new counter

--- scenarios/nsp/ingress/reach_ingress.go
+++ scenarios/nsp/ingress/reach_ingress.go
@@ -2,7 +2,6 @@ package ingress
 
 import (
 	"fmt"
-	"github.com/equinor/radix-cicd-canary/metrics"
 	nspMetrics "github.com/equinor/radix-cicd-canary/metrics/scenarios/nsp"
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/env"
 	httpUtils "github.com/equinor/radix-cicd-canary/scenarios/utils/http"
@@ -38,15 +37,15 @@ func getIngressForRadixCanaryApp(clusterFQDN string) string {
 // Success is a function after a call to Reach succeeds
 func Success(testName string) {
 	nspMetrics.AddIngressReachable()
-	metrics.AddTestSuccess(testName)
-	metrics.AddTestNoError(testName)
+	nspMetrics.AddTestSuccess(testName)
+	nspMetrics.AddTestNoError(testName)
 	logger.Infof("Test %s: SUCCESS", testName)
 }
 
 // Fail is a function after a call to Reach failed
 func Fail(testName string) {
 	nspMetrics.AddIngressUnreachable()
-	metrics.AddTestNoSuccess(testName)
-	metrics.AddTestError(testName)
+	nspMetrics.AddTestNoSuccess(testName)
+	nspMetrics.AddTestError(testName)
 	logger.Infof("Test %s: FAIL", testName)
 }
