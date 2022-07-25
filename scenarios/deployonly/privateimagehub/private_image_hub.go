dd9711de22232d7a13c81e1751aed1a1ca56d3e0
Nils Gustav Stråbø <nst@equinor.com>
2022-07-25 14:28:32 +0200

    use random sleep in deployonly when setting secret

--- scenarios/deployonly/privateimagehub/private_image_hub.go
+++ scenarios/deployonly/privateimagehub/private_image_hub.go
@@ -2,6 +2,8 @@ package privateimagehub
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/equinor/radix-cicd-canary/scenarios/utils/config"
 	envUtil "github.com/equinor/radix-cicd-canary/scenarios/utils/env"
@@ -14,6 +16,13 @@ var logger *log.Entry
 // Set runs tests related to private image hub. Expect canary2 to be built and deployed before test run
 func Set(env envUtil.Env, suiteName string) (bool, error) {
 	logger = log.WithFields(log.Fields{"Suite": suiteName})
+
+	// Due to a timing bug in Config Syncer (https://github.com/kubeops/config-syncer) that can happen
+	// when a new namespace is created and at the same time a secret that must be synced to the namespace is updated,
+	// the old "cached" secret from the nsSyncer overwrites the secret created by the secret informer's OnUpdate.
+	// Currently this timing bug is activly happening in the playground cluster almost every time this suite is executed.
+	// The random sleep (5-10 sec) will allow the nsSyncer in Config Syncer to update perform the initial sync before we update the secret.
+	time.Sleep(time.Duration(rand.Intn(10)+5) * time.Second)
 
 	err := privateimagehub.SetPassword(env, config.App3Name)
 	if err != nil {
