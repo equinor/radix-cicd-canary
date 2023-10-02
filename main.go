65007c8178578e1cf807732342835a2734d340c1
Richard Hagen <richard.hagen@bouvet.no>
2023-10-02 09:41:14 +0200

    cleanup log output (integer ms instead of floats)

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	logLevel := cfg.GetLogLevel()
 	pretty := cfg.GetPrettyPrint()
 	zerolog.SetGlobalLevel(logLevel)
+	zerolog.DurationFieldInteger = true
 	if pretty {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.TimeOnly})
 	}
