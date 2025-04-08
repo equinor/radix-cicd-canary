6d548e766b528b7191b3fc507d125005ae6b4949
Sergey Smolnikov <ssmol@equinor.com>
2025-04-08 15:40:47 +0200

    Fixed missing sub-pipeline steps with empty commitID build for canary-test-2

--- scenarios/utils/job/steps.go
+++ scenarios/utils/job/steps.go
@@ -1,19 +1,23 @@
 package job
 
 import (
+	"github.com/equinor/radix-cicd-canary/generated-client/radixapi/models"
 	"github.com/equinor/radix-common/utils"
 	"github.com/equinor/radix-common/utils/slice"
 )
 
 type ExpectedSteps interface {
 	Add(stepName string, components ...string) ExpectedSteps
+	AddForSubPipeline(stepName string, subPipelineTaskStep *models.SubPipelineTaskStep) ExpectedSteps
 	Count() int
 	HasStepWithComponent(stepName string, components []string) bool
+	HasStepWithSubPipelineTaskStep(stepName string, subPipelineTaskStep *models.SubPipelineTaskStep) bool
 }
 
 type expectedStep struct {
-	components []string
-	name       string
+	components          []string
+	name                string
+	subPipelineTaskStep *models.SubPipelineTaskStep
 }
 
 type expectedSteps struct {
@@ -31,6 +35,11 @@ func (es *expectedSteps) Add(stepName string, components ...string) ExpectedStep
 	return es
 }
 
+func (es *expectedSteps) AddForSubPipeline(stepName string, subPipelineTaskStep *models.SubPipelineTaskStep) ExpectedSteps {
+	es.steps = append(es.steps, expectedStep{name: stepName, subPipelineTaskStep: subPipelineTaskStep})
+	return es
+}
+
 func (es *expectedSteps) Count() int {
 	return len(es.steps)
 }
@@ -38,5 +47,16 @@ func (es *expectedSteps) Count() int {
 func (es *expectedSteps) HasStepWithComponent(stepName string, components []string) bool {
 	return slice.Any(es.steps, func(step expectedStep) bool {
 		return step.name == stepName && utils.ArrayEqualElements(step.components, components)
+	})
+}
+
+func (es *expectedSteps) HasStepWithSubPipelineTaskStep(stepName string, subPipelineTaskStep *models.SubPipelineTaskStep) bool {
+	if subPipelineTaskStep == nil {
+		panic("subPipelineTaskStep is nil")
+	}
+	return slice.Any(es.steps, func(step expectedStep) bool {
+		return step.name == stepName && step.subPipelineTaskStep != nil &&
+			step.subPipelineTaskStep.Environment == subPipelineTaskStep.Environment &&
+			*step.subPipelineTaskStep.PipelineName == *subPipelineTaskStep.PipelineName
 	})
 }
