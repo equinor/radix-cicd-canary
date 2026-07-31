328a0009304b6ef2553aa7862bf11e8f221cbb93
Richard Hagen <richard.hagen@gmail.com>
2026-07-31 16:21:13 +0200

    Updated generated files

--- generated-client/radixapi/models/application_registration.go
+++ generated-client/radixapi/models/application_registration.go
@@ -73,6 +73,10 @@ type ApplicationRegistration struct {
 	// Example: https://github.com/equinor/radix-canary-golang
 	// Required: true
 	Repository *string `json:"repository"`
+
+	// SharedSecret the shared secret of the webhook
+	// Required: true
+	SharedSecret *string `json:"sharedSecret"`
 }
 
 // Validate validates this application registration
@@ -120,6 +124,10 @@ func (m *ApplicationRegistration) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateRepository(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateSharedSecret(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -222,6 +230,15 @@ func (m *ApplicationRegistration) validateReaderAdUsers(formats strfmt.Registry)
 func (m *ApplicationRegistration) validateRepository(formats strfmt.Registry) error {
 
 	if err := validate.Required("repository", "body", m.Repository); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *ApplicationRegistration) validateSharedSecret(formats strfmt.Registry) error {
+
+	if err := validate.Required("sharedSecret", "body", m.SharedSecret); err != nil {
 		return err
 	}
 
